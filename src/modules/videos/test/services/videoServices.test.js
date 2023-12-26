ddbd146eda75367ca1fdd69fa0cbdcd563b5bbbe
Ygor Fortes <ygor1888@hotmail.com>
2023-12-26 17:50:39 -0300

    test: criado teste para findOne de ValitatorSchemaVideo e findOne de videoService

--- src/modules/videos/test/services/videoServices.test.js
+++ src/modules/videos/test/services/videoServices.test.js
@@ -5,7 +5,7 @@ import Sinon from 'sinon';
 import { VideoService } from "../../services/VideoService.js";
 import { ValitatorSchemaVideo } from '../../validators/ValitatorSchemaVideo.js';
 
-describe('validateFilters', ()=>{
+describe('Deve validar o findAll de ValitatorSchemaVideo', ()=>{
   let valitatorSchemaVideo;
 
   beforeEach(()=>{
@@ -25,7 +25,7 @@ describe('validateFilters', ()=>{
       url: 'https://www.w3schools.com/jsref/jsref_includes_array.asp',
     };
 
-    const resultado = await valitatorSchemaVideo.validateFilters(atribbutes);
+    const resultado = await valitatorSchemaVideo.findAll(atribbutes);
     expect(resultado).toEqual(expectAtribbutes);
   });
 
@@ -36,14 +36,11 @@ describe('validateFilters', ()=>{
       url: 'não é uma url',
     }; 
 
-    await expect(valitatorSchemaVideo.validateFilters(atribbutes)).rejects.toThrow('A url deve ser valida');
+    await expect(valitatorSchemaVideo.findAll(atribbutes)).rejects.toThrow('A url deve ser valida');
   });
-
-
- 
 });
 
-describe('Testando VideoService', ()=>{
+describe('Testando método findAll de VideoService', ()=>{
   let videoService;
   let videoRepositoryMock;
 
@@ -56,8 +53,8 @@ describe('Testando VideoService', ()=>{
     videoRepositoryMock.restore();
   });
 
-  it('Deve chamar todos os videos se filtro estiver nulo',async ()=>{
-    const filter = {titulo: null};
+  it('Deve chamar todos os videos se filtro estiver undefined',async ()=>{
+    const filter = {titulo: undefined};
 
     const expectedVideos = [
       {id: 1, titulo: 'teste', descricao: 'descrição teste', url: 'url teste'},
@@ -97,8 +94,130 @@ describe('Testando VideoService', ()=>{
   });
 });
 
+describe('Deve validar o metodo findOne de ValitatorSchemaVideo ', ()=>{
+  let valitatorSchemaVideo;
+  beforeEach(()=>{
+    valitatorSchemaVideo = new ValitatorSchemaVideo();
+  });
+
+  it('Deve retirar os espaços em branco do params', async()=>{
+    const elementId = {
+      id: 'b0be69fa-ebc1-48dc-9bc7-ef471bda71b8    '
+    };
+
+    const expectElementId = {
+      id: 'b0be69fa-ebc1-48dc-9bc7-ef471bda71b8'
+    };
+
+    const resultado = await valitatorSchemaVideo.findOne(elementId);
+    expect(resultado).toEqual(expectElementId);
+  });
+
+  it('O campo elementId do params deve ser um UUID', async()=>{
+    const elementId = {
+      id: 'não é um UUID'
+    };
+
+    await expect(valitatorSchemaVideo.findOne(elementId)).rejects.toThrow('O parâmetros elementId no params deve ser UUID válido.');
+  });
+
+  it('O campo elementId do params deve ser obrigatório', async()=>{
+    const elementId = {
+      id: null
+    };
+ 
+    await expect(valitatorSchemaVideo.findOne(elementId)).rejects.toThrow('O parâmetros id no params é obigatório.');
+  });
+
+});
+
+describe('testando método findOne de VideoService', ()=>{
+  let videoService;
+  let videoRepositoryMock;
+
+  beforeEach(()=>{
+    videoService = new VideoService();
+    videoRepositoryMock = Sinon.mock(videoService.videoRepository);
+  });
+  
+  afterEach(()=>{
+    videoRepositoryMock.restore();
+  });
+
+  it('Deve retorna um array vazio se não encontrar o video', async ()=>{
+    
+    const expectedVideo = [];
+
+    const elementId = {
+      id: 'b0be69fa-ebc1-48dc-9bca-ef471bda71b8'
+    };
+
+    videoRepositoryMock.expects('findOne')
+    .withExactArgs(elementId)
+    .resolves(expectedVideo);
+
+    const video = await videoService.findAll(elementId);
+
+    expect(video).toEqual(expectedVideo);
+  });
+
+  it('Deve retornar um video conforme o id', async()=>{
+    const expectedVideo = {
+      id: 'b0be69fa-ebc1-48dc-9bc7-ef471bda71b8',
+      titulo: 'titulo teste',
+      descricao: 'descricao teste',
+      url: 'url teste'
+    };
+
+    const elementId = {
+      id: 'b0be69fa-ebc1-48dc-9bc7-ef471bda71b8'
+    };
+
+    videoRepositoryMock.expects('findOne')
+    .withExactArgs(elementId)
+    .resolves(expectedVideo);
+
+    const video = await videoService.findOne(elementId);
+
+    expect(video).toEqual(expectedVideo);
+    expect(video.id).toEqual(expectedVideo.id);
+  });
+
+});
 
 
 
 
 
+
+
+//   videoRepositoryMock.expects('findAll')
+//   .withExactArgs(filter)
+//   .resolves(expectedVideos);
+
+//   const videos = await videoService.findAll(filter);
+//   videoRepositoryMock.verify();
+//   expect(videos).toEqual(expectedVideos);
+// });
+
+// it('Deve encontrar os videos de acordo com o filtro passado', async ()=>{
+//   const filter = {titulo: 'teste'};
+
+//   const allVideos = [
+//     {id: 1, titulo: 'teste', descricao: 'descrição teste', url: 'url teste'},
+//     {id: 2, titulo: 'teste 2', descricao: 'descrição test 2', url: 'url teste 2' },
+//     {id: 3, titulo: 'novo titulo', descricao: 'nova descricao', url: 'nova url' }
+//   ];
+
+//   const expectedVideos = allVideos.filter(video => video.titulo.includes(filter.titulo));
+
+
+//   videoRepositoryMock.expects('findAll')
+//   .withExactArgs(filter)
+//   .resolves(expectedVideos);
+
+
+//   const videos = await videoService.findAll(filter);
+//   videoRepositoryMock.verify();
+//   expect(videos).toEqual(expectedVideos);
+// });
