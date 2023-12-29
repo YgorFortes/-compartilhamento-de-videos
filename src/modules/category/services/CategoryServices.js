61367243c42e797d273f0ce586778c8e66fc99c7
Ygor Fortes <ygor1888@hotmail.com>
2023-12-29 18:04:07 -0300

    feat: adicionado método findOne em Controller, Repository, Services e Validators

--- src/modules/category/services/CategoryServices.js
+++ src/modules/category/services/CategoryServices.js
@@ -1,17 +1,37 @@
 import { CrudServiceUtils } from "../../../utils/crud/crudServiceUtils.js";
 import {CategoryRepository} from "../repository/CategoryRepository.js";
+import { ValidatorSchemaCategory } from "../validators/ValidatorSchemaCategory.js";
 
 
 export  class CategoryService extends CrudServiceUtils{
   constructor(){
     super();
-
     this.categoryRepository= new CategoryRepository();
+    this.validatorSchemaCategory = new ValidatorSchemaCategory();
   }
 
   async findAll(filter){
     try {
       const category = await this.categoryRepository.findAll(filter);
+
+      return category;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  async findOne(categoryId){
+    try {
+
+      await this.validatorSchemaCategory.findOne(categoryId);
+
+      const category = await this.categoryRepository.findOne(categoryId);
+
+      
+      if(!category){
+        return [];
+      }
 
       return category;
     } catch (error) {
--- src/modules/category/validators/ValidatorSchemaCategory.js
+++ src/modules/category/validators/ValidatorSchemaCategory.js
@@ -2,7 +2,7 @@ import Yup from 'yup';
 
 export class ValidatorSchemaCategory{
   async findAll(categoryQuery){
-    
+
     const cateogrySchemaFindAll = Yup.object({
       query: Yup.object().shape({
         titulo: Yup.string().trim().lowercase(),
@@ -12,6 +12,19 @@ export class ValidatorSchemaCategory{
     });
     
     const result =  await cateogrySchemaFindAll.fields.query.validate(categoryQuery);
+    return result;
+  }
+
+  async findOne(categoryParams){
+    const categorySchemaFindOne = Yup.object({
+      params: Yup.object().shape({
+        id: Yup.string().trim()
+        .required('O parâmetros id no params é obigatório.')
+        .uuid('O parâmetros elementId no params deve ser UUID válido.')
+      }),
+    });
+
+    const result = await categorySchemaFindOne.fields.params.validate(categoryParams);
     return result;
   }
 }
