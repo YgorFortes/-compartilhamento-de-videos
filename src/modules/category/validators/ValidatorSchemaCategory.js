4a97877ca3028b2b014433300ad17a04a9d86ce6
Ygor Fortes <ygor1888@hotmail.com>
2024-01-01 12:51:51 -0300

    feat: adicionado metodo de validações de create e delete

--- src/modules/category/validators/ValidatorSchemaCategory.js
+++ src/modules/category/validators/ValidatorSchemaCategory.js
@@ -27,4 +27,32 @@ export class ValidatorSchemaCategory{
     const result = await categorySchemaFindOne.fields.params.validate(categoryParams);
     return result;
   }
+
+  async create(categoryBody){
+    const categorySchemaCreate = Yup.object({
+      body: Yup.object().shape({
+        titulo:  Yup.string().trim().lowercase()
+        .required('O campo titulo é obrigatório.'),
+        cor: Yup.string().trim().lowercase()
+        .required('O campo cor é obrigatório.')
+      })
+    });
+
+    const result = await categorySchemaCreate.fields.body.validate(categoryBody);
+    return result;
+  };
+
+  async delete(categoryParams){
+    const categorySchemaFindOne = Yup.object({
+      params: Yup.object().shape({
+        id: Yup.string().trim()
+        .required('O parâmetros id no params é obigatório.')
+        .uuid('O parâmetros elementId no params deve ser UUID válido.')
+      }),
+    });
+
+    const result = await categorySchemaFindOne.fields.params.validate(categoryParams);
+    return result;
+  }
+
 }
