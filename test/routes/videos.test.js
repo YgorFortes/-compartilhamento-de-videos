2f1720240d65ace5f23d0073f3f06b430a87aae8
Ygor Fortes <ygor1888@hotmail.com>
2024-01-03 16:16:16 -0300

    test: ajutado testes para esteja com a relação de 1 para n com categoria em dia

--- test/routes/videos.test.js
+++ test/routes/videos.test.js
@@ -72,11 +72,20 @@ describe('GET /videos', () => {
 
 describe('POST /videos',()=>{
   it('Deve criar um novo video e ter status code 201', async()=>{
+    const categoria = {
+      titulo: 'titulo',
+      cor: 'cor'
+    };
+    
+    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+
+    const novaCategoria = await respostaCategoria.body;
 
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
-      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki'
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: novaCategoria.id
     };
 
 
@@ -87,10 +96,11 @@ describe('POST /videos',()=>{
 
     expect(resposta.statusCode).toEqual(201);
     expect(typeof resposta.body).toBe('object');
-    expect(resposta.body).toHaveProperty('titulo', 'descricao', 'url');
+    expect(resposta.body).toHaveProperty('titulo', 'descricao', 'url', 'categoriaId');
     expect(resposta.body).toMatchObject(video);
 
     await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCriado.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
   
   });
 
@@ -105,14 +115,45 @@ describe('POST /videos',()=>{
 
     expect(resposta.statusCode).toEqual(400);
   });
+
+  it('Deve lançar um erroe ter status code 404', async()=>{
+
+    const video = {
+      titulo: 'titulo',
+      descricao: 'descricao',
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: '6dab268e-c52e-45ee-867b-a65ff3030ddd'
+    };
+
+
+    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    .send(video);
+
+
+    expect(resposta.statusCode).toEqual(404);
+    expect(typeof resposta.body).toBe('object');
+    expect(resposta.body).toStrictEqual({ mensagem: 'Categoria não encontrado.' });
+  
+  });
 });
 
 describe('PATH /videos', ()=>{
   it('Deve editar um video criado e status code 200',async()=>{
+
+    const categoria = {
+      titulo: 'titulo',
+      cor: 'cor'
+    };
+    
+    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+
+    const novaCategoria = await respostaCategoria.body;
+
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
-      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki'
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: novaCategoria.id
     };
 
     const newInfoVideo= {
@@ -131,6 +172,7 @@ describe('PATH /videos', ()=>{
     expect(respostaEditar.statusCode).toEqual(200);
     expect(respostaEditar.body.descricao).toEqual(newInfoVideo.descricao);
     await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCriado.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
   });
 
   it('Deve retornar erro 404', async()=>{
@@ -149,10 +191,19 @@ describe('PATH /videos', ()=>{
 
 describe('DELETE /videos', ()=>{
   it('Deve deletar o video, mandar status code 200 e uma mensagem de sucesso', async()=>{
+    const categoria = {
+      titulo: 'titulo',
+      cor: 'cor'
+    };
+    
+    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+    const novaCategoria = await respostaCategoria.body;
+
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
-      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki'
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: novaCategoria.id
     };
   
     const expectResult = {
@@ -168,10 +219,11 @@ describe('DELETE /videos', ()=>{
 
     expect(responseDelete.body).toEqual(expectResult);
     expect(responseDelete.statusCode).toEqual(200);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
 
   });
 
-  it('Deve lanaçar um erro status 404', async()=>{
+  it('Deve lançar um erro status 404', async()=>{
     const responseDelete = await request('http://localhost:3000/api/v1/').delete(`/videos/6076344c-a832-4e57-b6d4-d2949fc1d67d`);
     expect(responseDelete.statusCode).toEqual(404);
   });
