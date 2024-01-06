e36bc5a0b5f2ad36602d0da3582f4a0ce7832f3f
Ygor Fortes <ygor1888@hotmail.com>
2024-01-06 00:42:01 -0300

    test: Adionado test de integração em GET /videos relacionadas aos filters e a paginação

--- test/routes/videos.test.js
+++ test/routes/videos.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable import/no-extraneous-dependencies */
 import { describe, expect, it, afterAll, beforeAll } from '@jest/globals';
 import request from 'supertest';
@@ -14,13 +15,13 @@ afterAll(() => {
 
 describe('GET /videos', () => {
   it('Deve retornar todos os videos', async () => {
-    const resposta = await request('http://localhost:3000/api/v1/').get('/videos');
+    const reponse = await request('http://localhost:3000/api/v1/').get('/videos');
    
-    expect(resposta.status).toBe(200);
-    expect(resposta.headers['content-type']).toContain('application/json');
+    expect(reponse.status).toBe(200);
+    expect(reponse.headers['content-type']).toContain('application/json');
 
 
-    const videos = await resposta.body;
+    const videos = await reponse.body;
     expect(typeof videos).toBe('object');
     expect(videos[0]).toHaveProperty('titulo', 'descricao', 'url');
     expect(videos.length).toBeGreaterThanOrEqual(1);
@@ -28,95 +29,218 @@ describe('GET /videos', () => {
   });
 
   it('Deve dar erro 404 ao errar o link', async()=>{
-    const resposta = await request('http://localhost:3000/api/v1/').get('/video');
+    const response = await request('http://localhost:3000/api/v1/').get('/video');
    
-    expect(resposta.status).toBe(404);
+    expect(response.status).toBe(404);
   });
 
-  it('Deve voltar um array vazio quando filter não for encontrado', async()=>{
-    const resposta = await request('http://localhost:3000/api/v1/').get('/videos/?titulo=nariz');
+  it('Deve retornar categorias de acordo com o filtro', async()=>{
+
+    const category = {
+      titulo: 'titulo',
+      cor: 'cor'
+    };
+    
+    const reponseCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
+
+    const newCategory = await reponseCategory.body;
+
+    const video = {
+      titulo: 'titulo',
+      descricao: 'descricao',
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: newCategory.id
+    };
+
+
+    const responsePostVideo = await request('http://localhost:3000/api/v1/').post('/videos')
+    .send(video);
+    const newVideo = responsePostVideo.body;
+
+    const video2 = {
+      titulo: 'não tem nada',
+      descricao: 'descricao',
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: newCategory.id
+    };
+
+
+    const responsePostVideo2 = await request('http://localhost:3000/api/v1/').post('/videos')
+    .send(video2);
+    const newVideo2 = responsePostVideo2.body;
+
+    const video3 = {
+      titulo: 'tem titulo no nome, mas é diferente',
+      descricao: 'descricao',
+      url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+      categoriaId: newCategory.id
+    };
+
+
+    const responsePostVideo3 = await request('http://localhost:3000/api/v1/').post('/videos')
+    .send(video3);
+    const newVideo3 = responsePostVideo3.body;
    
-    expect(resposta.status).toBe(200);
-    expect(resposta.headers['content-type']).toContain('application/json');
+   
 
 
-    const videos = await resposta.body;
+    const responseGetVideo = await request('http://localhost:3000/api/v1/').get('/videos?titulo=titulo');
+   
+    expect(responseGetVideo.status).toBe(200);
+    expect(responseGetVideo.headers['content-type']).toContain('application/json');
+
+    const videos = await responseGetVideo.body;
+
+    expect(typeof videos).toBe('object');
+
+    expect(videos[0]).toHaveProperty('id','titulo', 'cor');
+
+    expect(videos.length).toBeGreaterThanOrEqual(1);
+    
+    videos.forEach((videoSingle)=>expect(videoSingle.titulo).toContain('titulo'));
+
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo2.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo3.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
+  });
+
+  it('Deve voltar um array vazio quando o parametro do filtro não for encontrado ', async()=>{
+    const reponse = await request('http://localhost:3000/api/v1/').get('/videos/?titulo=aaaaaaa');
+   
+    expect(reponse.status).toBe(200);
+    expect(reponse.headers['content-type']).toContain('application/json');
+
+
+    const videos = await reponse.body;
     expect(videos).toEqual([]);
   });
 
   it('Deve retorna um video conforme seu id', async()=>{
-    const resposta = await request('http://localhost:3000/api/v1/').get('/videos');
+    const response = await request('http://localhost:3000/api/v1/').get('/videos');
    
-    expect(resposta.status).toBe(200);
-    expect(resposta.headers['content-type']).toContain('application/json');
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toContain('application/json');
 
 
-    const videos = await resposta.body;
+    const videos = await response.body;
 
-    const resposta2 = await request('http://localhost:3000/api/v1/').get(`/videos/${videos[0].id}`);
-    const video = await resposta2.body;
+    const response2 = await request('http://localhost:3000/api/v1/').get(`/videos/${videos[0].id}`);
+    const video = await response2.body;
 
     expect(typeof video).toBe('object');
+    expect(video).toHaveProperty('id','titulo', 'descricao','url');
 
   });
 
-  it('Deve retornar um array vazio', async()=>{
-    const resposta2 = await request('http://localhost:3000/api/v1/').get(`/videos/795f636b-0fd3-4e74-92ad-afa7a7ea1100`);
-    const video = await resposta2.body;
+  it('Deve retornar um array vazio caso não exista o video cadastrado', async()=>{
+    const reponse = await request('http://localhost:3000/api/v1/').get(`/videos/795f636b-0fd3-4e74-92ad-afa7a7ea1100`);
+    const video = await reponse.body;
 
-    expect(resposta2.status).toBe(200);
+    expect(reponse.status).toBe(200);
     expect(video).toEqual([]);
+  });
+
+  it('Deve paginar corretamente ', async()=>{
+
+    const category = {
+      titulo: 'titulo',
+      cor: 'cor'
+    };
+    
+    const reponsePostCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
+    const newCategory = reponsePostCategory.body;
+    
+    let respostaPostArray = [];
+    const promisses = [];
+
+    const page = 1;
+    for( let i = 0; i <10; i++ ){
+      const video = {
+        titulo: 'titulo',
+        descricao: 'descricao',
+        url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
+        categoriaId: newCategory.id
+      };
+  
+      const responsePostVideo =  request('http://localhost:3000/api/v1/').post('/videos')
+      .send(video);
+      promisses.push(responsePostVideo);
+    }
+
+    respostaPostArray = await Promise.all(promisses);
+
+    const responseGetVideos = await request('http://localhost:3000/api/v1/').get(`/videos?page=${page}`);
+    
+    const videos = await responseGetVideos.body;
+
+
+    expect(videos).toHaveLength(5);
+
+    const responseGetVideos2 = await request('http://localhost:3000/api/v1/').get(`/videos?page=2`);
+    
+    const videos2 = await responseGetVideos2.body;
+
+
+    videos2.forEach((video, index) => {
+      expect(video.id).not.toEqual(videos[index].id);
+    });
+
+    const deletePost = respostaPostArray.map((respostaPost)=>request('http://localhost:3000/api/v1/').delete(`/videos/${respostaPost.body.id}`));
+    await Promise.all(deletePost);
+
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
   });
 
 });
 
 describe('POST /videos',()=>{
   it('Deve criar um novo video e ter status code 201', async()=>{
-    const categoria = {
+    const category = {
       titulo: 'titulo',
       cor: 'cor'
     };
     
-    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+    const reponseCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
 
-    const novaCategoria = await respostaCategoria.body;
+    const newCategory = await reponseCategory.body;
 
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
       url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
-      categoriaId: novaCategoria.id
+      categoriaId: newCategory.id
     };
 
 
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const responsePostVideo = await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
 
-    const videoCriado = resposta.body;
+    const newVideo = responsePostVideo.body;
 
-    expect(resposta.statusCode).toEqual(201);
-    expect(typeof resposta.body).toBe('object');
-    expect(resposta.body).toHaveProperty('titulo', 'descricao', 'url', 'categoriaId');
-    expect(resposta.body).toMatchObject(video);
 
-    await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCriado.id}`);
-    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
-  
+    expect(responsePostVideo.statusCode).toEqual(201);
+    expect(typeof newVideo).toBe('object');
+    expect(newVideo).toMatchObject(video);
+
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
+    
   });
 
-  it('Deve retornar erro 400 ao usuário não colocar todos os campos requeridos', async()=>{
+  it('Deve dar erro 404 caso não coloquem todos os campos obrigátorios no  body', async()=>{
     const video = {
       descricao: 'descricao',
       url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki'
     };
 
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const response = await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
 
-    expect(resposta.statusCode).toEqual(400);
+    expect(response.statusCode).toEqual(400);
   });
 
-  it('Deve lançar um erroe ter status code 404', async()=>{
+  it('Deve lançar um erroe ter status code 404 caso não encontre a categoria no body', async()=>{
 
     const video = {
       titulo: 'titulo',
@@ -126,13 +250,13 @@ describe('POST /videos',()=>{
     };
 
 
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const responsePostVideo = await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
 
 
-    expect(resposta.statusCode).toEqual(404);
-    expect(typeof resposta.body).toBe('object');
-    expect(resposta.body).toStrictEqual({ mensagem: 'Categoria não encontrado.' });
+    expect(responsePostVideo.statusCode).toEqual(404);
+    expect(typeof responsePostVideo.body).toBe('object');
+    expect(responsePostVideo.body).toStrictEqual({ mensagem: 'Categoria não encontrado.' });
   
   });
 });
@@ -140,20 +264,20 @@ describe('POST /videos',()=>{
 describe('PATH /videos', ()=>{
   it('Deve editar um video criado e status code 200',async()=>{
 
-    const categoria = {
+    const category = {
       titulo: 'titulo',
       cor: 'cor'
     };
     
-    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+    const responsePostCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
 
-    const novaCategoria = await respostaCategoria.body;
+    const newCategory = await responsePostCategory.body;
 
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
       url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
-      categoriaId: novaCategoria.id
+      categoriaId: newCategory.id
     };
 
     const newInfoVideo= {
@@ -161,35 +285,37 @@ describe('PATH /videos', ()=>{
     };
 
 
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const responsePostVideo= await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
 
-    const videoCriado = await resposta.body;
+    const newVideo = await responsePostVideo.body;
 
-    const respostaEditar = await request('http://localhost:3000/api/v1/').patch(`/videos/${videoCriado.id}`)
+    const responsePatchVideo= await request('http://localhost:3000/api/v1/').patch(`/videos/${newVideo.id}`)
     .send(newInfoVideo);
 
-    expect(respostaEditar.statusCode).toEqual(200);
-    expect(respostaEditar.body.descricao).toEqual(newInfoVideo.descricao);
-    await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCriado.id}`);
-    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
+    expect(responsePatchVideo.statusCode).toEqual(200);
+    expect(responsePatchVideo.body.descricao).toEqual(newInfoVideo.descricao);
+    expect(responsePatchVideo.body.categoriaId).toEqual(video.categoriaId);
+
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
   });
 
   it('De dar erro 404 quando não encontrado categoria', async()=>{
-    const categoria = {
+    const category = {
       titulo: 'titulo',
       cor: 'cor'
     };
     
-    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
+    const responsePostCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
 
-    const novaCategoria = await respostaCategoria.body;
+    const newCategory = await responsePostCategory.body;
 
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
       url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
-      categoriaId: novaCategoria.id
+      categoriaId: newCategory.id
     };
 
     const newInfoVideo= {
@@ -197,18 +323,18 @@ describe('PATH /videos', ()=>{
     };
 
 
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const responsePostVideo = await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
 
-    const videoCriado = await resposta.body;
+    const newVideo = await responsePostVideo.body;
 
-    const respostaEditar = await request('http://localhost:3000/api/v1/').patch(`/videos/${videoCriado.id}`)
+    const responsePatchVideo = await request('http://localhost:3000/api/v1/').patch(`/videos/${newVideo.id}`)
     .send(newInfoVideo);
 
-    expect(respostaEditar.statusCode).toEqual(404);
-    expect(respostaEditar.body.descricao).toEqual(newInfoVideo.descricao);
-    await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCriado.id}`);
-    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
+    expect(responsePatchVideo.statusCode).toEqual(404);
+    expect(responsePatchVideo.body.descricao).toEqual(newInfoVideo.descricao);
+    await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo.id}`);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
   });
 
   it('Deve retornar erro 404', async()=>{
@@ -227,41 +353,42 @@ describe('PATH /videos', ()=>{
 
 describe('DELETE /videos', ()=>{
   it('Deve deletar o video, mandar status code 200 e uma mensagem de sucesso', async()=>{
-    const categoria = {
+    const category = {
       titulo: 'titulo',
       cor: 'cor'
     };
     
-    const respostaCategoria = await request('http://localhost:3000/api/v1/').post('/categorias').send(categoria);
-    const novaCategoria = await respostaCategoria.body;
+    const responsePostCategory = await request('http://localhost:3000/api/v1/').post('/categorias').send(category);
+    const newCategory = await responsePostCategory.body;
 
     const video = {
       titulo: 'titulo',
       descricao: 'descricao',
       url: 'https://www.youtube.com/watch?v=w6yGFi0dZeA&ab_channel=AndreOkazaki',
-      categoriaId: novaCategoria.id
+      categoriaId: newCategory.id
     };
   
     const expectResult = {
-      message: 'Video deletado com sucesso',
+      mensagem: 'Video deletado com sucesso',
     };
   
   
-    const resposta = await request('http://localhost:3000/api/v1/').post('/videos')
+    const responsePostVideo = await request('http://localhost:3000/api/v1/').post('/videos')
     .send(video);
-    const videoCreated = await resposta.body;
 
-    const responseDelete = await request('http://localhost:3000/api/v1/').delete(`/videos/${videoCreated.id}`);
+    const newVideo = await responsePostVideo.body;
 
-    expect(responseDelete.body).toEqual(expectResult);
-    expect(responseDelete.statusCode).toEqual(200);
-    await request('http://localhost:3000/api/v1/').delete(`/categorias/${novaCategoria.id}`);
+    const responseDeleteVideo = await request('http://localhost:3000/api/v1/').delete(`/videos/${newVideo.id}`);
+
+    expect(responseDeleteVideo.body).toEqual(expectResult);
+    expect(responseDeleteVideo.statusCode).toEqual(200);
+    await request('http://localhost:3000/api/v1/').delete(`/categorias/${newCategory.id}`);
 
   });
 
-  it('Deve lançar um erro status 404', async()=>{
-    const responseDelete = await request('http://localhost:3000/api/v1/').delete(`/videos/6076344c-a832-4e57-b6d4-d2949fc1d67d`);
-    expect(responseDelete.statusCode).toEqual(404);
+  it('Deve lançar um erro status 404 quando video não encontrado', async()=>{
+    const responseDeleteVideo = await request('http://localhost:3000/api/v1/').delete(`/videos/6076344c-a832-4e57-b6d4-d2949fc1d67d`);
+    expect(responseDeleteVideo.statusCode).toEqual(404);
   });
 });
 
