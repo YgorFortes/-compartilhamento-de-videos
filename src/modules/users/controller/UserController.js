587dcea79d8556d0561d407ca2b8d38c634f3940
Ygor Fortes <ygor1888@hotmail.com>
2024-01-10 16:23:41 -0300

    feat: adicionado metodo create em UserController

--- src/modules/users/controller/UserController.js
+++ src/modules/users/controller/UserController.js
@@ -1,19 +1,23 @@
 import { CrudControllerUtils } from "../../../utils/crud/crudControllerUtils.js";
 import { AuthService } from "../services/AuthService.js";
 import { ValidatorSchemaAuth } from "../validators/ValidatorSchemaAuth.js";
+import { ValidatorSchemaUser } from "../validators/ValidatorSchemaUser.js";
+import { UserService } from "../services/UserService.js";
 
 export class UserController extends CrudControllerUtils {
   constructor(){
     super();
     this.authService = new AuthService();
-    this.ValidatorSchemaAuth = new ValidatorSchemaAuth();
+    this.userService = new UserService();
+    this.validatorSchemaAuth = new ValidatorSchemaAuth();
+    this.validatorSchemaUser = new ValidatorSchemaUser();
     this.setupRouter(this.login());
   }
 
   login (){
     this.router.post('/login', async (req, res, next)=>{
       try {
-        const userValidated = await this.ValidatorSchemaAuth.login(req.body);
+        const userValidated = await this.validatorSchemaAuth.login(req.body);
         
         const userToken = await this.authService.login(userValidated);
         return res.status(200).send(userToken);
@@ -23,6 +27,21 @@ export class UserController extends CrudControllerUtils {
     });
 
 
+  }
+
+  create () {
+     this.router.post('/cadastrar', async(req, res, next)=>{
+
+      try {
+        const userDataValidate = await this.validatorSchemaUser.create(req.body);
+        
+        const newUser = await this.userService.create(userDataValidate);
+        return res.status(200).send(newUser);
+      } catch (error) {
+        next(error);
+      }
+
+     });
   }
   
   
