afbecc0d6f53d9b2ab21c888d4152113169accca
Ygor Fortes <ygor1888@hotmail.com>
2024-01-10 16:28:19 -0300

    feat: criado nova classe para lidar com o crud do user, e criado metodo create, createHashPassword e findByEmailOrLogin em UserService

---
+++ src/modules/users/services/UserService.js
@@ -0,0 +1,65 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import bcrypt from 'bcrypt';
+import { CrudServiceUtils } from "../../../utils/crud/crudServiceUtils.js";
+import { ValidatorSchemaUser } from '../validators/ValidatorSchemaUser.js'; 
+import { UserRepository } from "../repository/UserRepository.js";
+import { CustomError } from "../../app/erros/CustomError.js";
+import { UtilsUser } from '../utils/UtilsUser.js';
+
+export class UserService extends CrudServiceUtils {
+  constructor(){
+    super();
+    this.userRepository = new UserRepository();
+    this.validatorSchemaUser = new ValidatorSchemaUser();
+    this.utilsUser = new UtilsUser();
+  }
+
+
+  async create(userData){
+    const {login,email , senha} = userData;
+    try {
+      await this.validatorSchemaUser.create(userData);
+      
+      const emailOrLoginExist = await this.findByEmailOrLogin(login, email);
+
+      if(emailOrLoginExist){
+        throw new CustomError('Email ou login já cadastrado.',409);
+      }
+
+      const passwordHash = await this.createHashPassword(senha);
+
+      const newUser = await this.userRepository.create({login, email, senha: passwordHash});
+
+      const token = await this.utilsUser.createToken(newUser.id);
+
+      return {usuario: newUser, token};
+    } catch (error) {
+      CustomError.checkAndThrowError(error);
+    }
+  }
+
+  async createHashPassword(senha){
+    const salt = await bcrypt.genSalt(12);
+
+    const passwordHash = await bcrypt.hash(senha, salt);
+
+    return passwordHash;
+  }
+
+  
+  async findByEmailOrLogin(login , email){
+    if(login){
+      const userByLogin = await this.userRepository.findByLogin(login);
+      if(userByLogin){
+        return userByLogin; 
+      }
+    }
+
+    if(email){
+      const userByEmail = await this.userRepository.findByEmail(email);
+      if(userByEmail){
+        return userByEmail; 
+      }
+    }
+  } 
+}
