4947939449d6332f9ffd17b706c2819c69357739
Ygor Fortes <ygor1888@hotmail.com>
2024-01-10 21:29:00 -0300

    feat: criado removeToken e tokenExpired em Middleware TokenVerificationMiddleware

--- src/modules/app/middlewares/TokenVerificationMiddleware.js
+++ src/modules/app/middlewares/TokenVerificationMiddleware.js
@@ -2,20 +2,41 @@
 import jsonwebtoken from 'jsonwebtoken';
 import { CustomError } from "../erros/CustomError.js";
 
+const removedTokens = [];
 export class TokenVerificationMiddleware  {
+ 
   static checkAuthToken (req, res, next){
     const tokenHeader = req.headers['authorization'];
     const token = tokenHeader && tokenHeader.split(' ')[1];
+
     if(!token){
       throw new CustomError('Token não existe',401);
     }
+
+    TokenVerificationMiddleware.tokenExpired(token);
+
     try {
+      
       const secretKey = process.env.SECRET;
       jsonwebtoken.verify(token, secretKey);
-    
       next();
+     
     } catch (error) {
+      console.log(error);
       throw new CustomError('Token inválido',401);
+    }
+  }
+
+  static removeToken(req, res, next){
+    const tokenHeader = req.headers['authorization'];
+    const token = tokenHeader && tokenHeader.split(' ')[1];
+    removedTokens.push(token);
+    next();
+  }
+
+  static tokenExpired(token){
+    if(removedTokens.includes(token)){
+      throw new CustomError('Token expirado',401);
     }
   }
 }
